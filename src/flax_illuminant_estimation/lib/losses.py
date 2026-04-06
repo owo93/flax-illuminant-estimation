c707e4bafd04c4ced5c6b960f62d81f973b8ab1e
owo93 <iztyle30@gmail.com>
2026-04-06 19:32:30 +0700

    fix: compute cosine similarity with optax.losses

--- src/flax_illuminant_estimation/lib/losses.py
+++ src/flax_illuminant_estimation/lib/losses.py
@@ -1,24 +1,21 @@
 import jax
 import jax.numpy as jnp
+import optax
 
 
 @jax.jit
-def angular_error(pred, target):
-    cos_sim = jnp.sum(pred * target, axis=-1) / (
-        jnp.linalg.norm(pred, axis=-1) * jnp.linalg.norm(target, axis=-1) + 1e-8
-    )
-    return jnp.arccos(jnp.clip(cos_sim, -1.0, 1.0))
+def angular_error(pred, target, epsilon: float = 1e-8):
+    cos_sim = optax.losses.cosine_similarity(pred, target, epsilon=epsilon)
+    return jnp.arccos(jnp.clip(cos_sim, -1.0 + epsilon, 1.0 - epsilon))
 
 
 # https://doi.org/10.1109/TPAMI.2016.2582171
-def reproduction_angular_error(image, pred, gt):
-    rendered_pred = image / (pred + 1e-8)
-    rendered_gt = image / (gt + 1e-8)
+def reproduction_angular_error(image, pred, gt, epsilon: float = 1e-8):
+    rendered_pred = image / (pred + epsilon)
+    rendered_gt = image / (gt + epsilon)
 
     rgb_pred = jnp.sum(rendered_pred, axis=(0, 1))
     rgb_gt = jnp.sum(rendered_gt, axis=(0, 1))
 
-    cos_sim = jnp.dot(rgb_pred, rgb_gt) / (
-        jnp.linalg.norm(rgb_pred) * jnp.linalg.norm(rgb_gt) + 1e-8
-    )
-    return jnp.arccos(jnp.clip(cos_sim, -1.0, 1.0))
+    cos_sim = optax.losses.cosine_similarity(rgb_pred[None], rgb_gt[None], epsilon=epsilon)
+    return jnp.arccos(jnp.clip(cos_sim, -1.0 + epsilon, 1.0 - epsilon))
