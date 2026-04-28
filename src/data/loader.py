c1283b1b35da65e8737092de74fa6fda878c576c
owo93 <iztyle30@gmail.com>
2026-04-28 22:53:22 +0700

    feat: load_dataset() using HF

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,12 +1,8 @@
-# Load a simplecube++ dataset as train/test split in jnp.array
-import csv
-from pathlib import Path
-
 import jax
 import jax.numpy as jnp
+from datasets import load_dataset
 from jax import lax, random
 from PIL import Image
-from tqdm import tqdm
 
 
 @jax.jit
@@ -38,8 +34,7 @@ batched_augment = jax.jit(jax.vmap(augment, in_axes=(0, 0)))
 
 
 class SimpleCubePPDataset:
-    def __init__(self, split, root=None, seed=42, img_size=224):
-        self.root = Path(__file__).parent / "SimpleCube++"
+    def __init__(self, split, seed=42, img_size=224):
         self.split = split
         self.augment = split == "train"
         self.key = random.key(seed)
@@ -47,35 +42,19 @@ class SimpleCubePPDataset:
         self.img_size = img_size
 
     def _load_split(self, split):
-        split_root = self.root / split
-        img_dir = split_root / "PNG"
-        gt_path = split_root / "gt.csv"
-
-        samples = []
-
-        with open(gt_path, "r", newline="") as f:
-            total_rows = sum(1 for _ in csv.DictReader(f))
-
-        with open(gt_path, "r", newline="") as f:
-            reader = csv.DictReader(f)
-            for row in tqdm(reader, total=total_rows, ncols=80, desc=f"Loading {split} data"):
-                filename = row["image"]
-                illum = jnp.array([row["mean_r"], row["mean_g"], row["mean_b"]], dtype=jnp.float32)
-
-                samples.append({"image_path": img_dir / f"{filename}.png", "illuminant": illum})
-
-        return samples
+        return load_dataset("owo93/SimpleCubePP", split=split)
 
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, idx):
         sample = self.samples[idx]
-        img = Image.open(sample["image_path"]).convert("RGB")
+        img = sample["image"].convert("RGB")
         img = img.resize((self.img_size, self.img_size), Image.Resampling.LANCZOS)
         img = jnp.array(img, dtype=jnp.float32) / 255.0
+        illum = jnp.array(sample["illuminant"], dtype=jnp.float32)
 
-        return img, sample["illuminant"]
+        return img, illum
 
     def batches(self, batch_size: int, shuffle=True):
         self.key, shuffle_key = random.split(self.key)
